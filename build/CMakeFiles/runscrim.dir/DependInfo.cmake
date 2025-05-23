fd0e74f98898509745b1c7fdb33abd9e04c39dae
matildeoliveirasilvafeup <up202305722@up.pt>
2025-05-23 23:43:01 +0100

    comentarios finais

---
+++ build/CMakeFiles/runscrim.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/diana/CLionProjects/Prog_RGB_333/main/RunScrim.cpp" "CMakeFiles/runscrim.dir/main/RunScrim.cpp.o" "gcc" "CMakeFiles/runscrim.dir/main/RunScrim.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Color.cpp" "CMakeFiles/runscrim.dir/src/Color.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Color.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command.cpp" "CMakeFiles/runscrim.dir/src/Command.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Add.cpp" "CMakeFiles/runscrim.dir/src/Command/Add.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Add.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Blank.cpp" "CMakeFiles/runscrim.dir/src/Command/Blank.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Blank.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Chain.cpp" "CMakeFiles/runscrim.dir/src/Command/Chain.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Chain.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Crop.cpp" "CMakeFiles/runscrim.dir/src/Command/Crop.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Crop.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Fill.cpp" "CMakeFiles/runscrim.dir/src/Command/Fill.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Fill.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/HMirror.cpp" "CMakeFiles/runscrim.dir/src/Command/HMirror.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/HMirror.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Invert.cpp" "CMakeFiles/runscrim.dir/src/Command/Invert.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Invert.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Move.cpp" "CMakeFiles/runscrim.dir/src/Command/Move.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Move.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Open.cpp" "CMakeFiles/runscrim.dir/src/Command/Open.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Open.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Replace.cpp" "CMakeFiles/runscrim.dir/src/Command/Replace.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Replace.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Resize.cpp" "CMakeFiles/runscrim.dir/src/Command/Resize.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Resize.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/RotateLeft.cpp" "CMakeFiles/runscrim.dir/src/Command/RotateLeft.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/RotateLeft.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/RotateRight.cpp" "CMakeFiles/runscrim.dir/src/Command/RotateRight.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/RotateRight.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Save.cpp" "CMakeFiles/runscrim.dir/src/Command/Save.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Save.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/ScaleUp.cpp" "CMakeFiles/runscrim.dir/src/Command/ScaleUp.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/ScaleUp.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Slide.cpp" "CMakeFiles/runscrim.dir/src/Command/Slide.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/Slide.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/ToGrayScale.cpp" "CMakeFiles/runscrim.dir/src/Command/ToGrayScale.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/ToGrayScale.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/VMirror.cpp" "CMakeFiles/runscrim.dir/src/Command/VMirror.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Command/VMirror.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Image.cpp" "CMakeFiles/runscrim.dir/src/Image.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Image.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Logger.cpp" "CMakeFiles/runscrim.dir/src/Logger.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Logger.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/PNG.cpp" "CMakeFiles/runscrim.dir/src/PNG.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/PNG.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Scrim.cpp" "CMakeFiles/runscrim.dir/src/Scrim.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Scrim.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/ScrimParser.cpp" "CMakeFiles/runscrim.dir/src/ScrimParser.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/ScrimParser.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Utils.cpp" "CMakeFiles/runscrim.dir/src/Utils.cpp.o" "gcc" "CMakeFiles/runscrim.dir/src/Utils.cpp.o.d"
+  "" "runscrim" "gcc" "CMakeFiles/runscrim.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tester.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/diana/CLionProjects/Prog_RGB_333/main/Tester.cpp" "CMakeFiles/tester.dir/main/Tester.cpp.o" "gcc" "CMakeFiles/tester.dir/main/Tester.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Color.cpp" "CMakeFiles/tester.dir/src/Color.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Color.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command.cpp" "CMakeFiles/tester.dir/src/Command.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Add.cpp" "CMakeFiles/tester.dir/src/Command/Add.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Add.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Blank.cpp" "CMakeFiles/tester.dir/src/Command/Blank.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Blank.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Chain.cpp" "CMakeFiles/tester.dir/src/Command/Chain.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Chain.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Crop.cpp" "CMakeFiles/tester.dir/src/Command/Crop.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Crop.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Fill.cpp" "CMakeFiles/tester.dir/src/Command/Fill.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Fill.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/HMirror.cpp" "CMakeFiles/tester.dir/src/Command/HMirror.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/HMirror.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Invert.cpp" "CMakeFiles/tester.dir/src/Command/Invert.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Invert.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Move.cpp" "CMakeFiles/tester.dir/src/Command/Move.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Move.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Open.cpp" "CMakeFiles/tester.dir/src/Command/Open.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Open.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Replace.cpp" "CMakeFiles/tester.dir/src/Command/Replace.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Replace.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Resize.cpp" "CMakeFiles/tester.dir/src/Command/Resize.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Resize.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/RotateLeft.cpp" "CMakeFiles/tester.dir/src/Command/RotateLeft.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/RotateLeft.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/RotateRight.cpp" "CMakeFiles/tester.dir/src/Command/RotateRight.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/RotateRight.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Save.cpp" "CMakeFiles/tester.dir/src/Command/Save.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Save.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/ScaleUp.cpp" "CMakeFiles/tester.dir/src/Command/ScaleUp.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/ScaleUp.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/Slide.cpp" "CMakeFiles/tester.dir/src/Command/Slide.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/Slide.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/ToGrayScale.cpp" "CMakeFiles/tester.dir/src/Command/ToGrayScale.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/ToGrayScale.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Command/VMirror.cpp" "CMakeFiles/tester.dir/src/Command/VMirror.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Command/VMirror.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Image.cpp" "CMakeFiles/tester.dir/src/Image.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Image.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Logger.cpp" "CMakeFiles/tester.dir/src/Logger.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Logger.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/PNG.cpp" "CMakeFiles/tester.dir/src/PNG.cpp.o" "gcc" "CMakeFiles/tester.dir/src/PNG.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Scrim.cpp" "CMakeFiles/tester.dir/src/Scrim.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Scrim.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/ScrimParser.cpp" "CMakeFiles/tester.dir/src/ScrimParser.cpp.o" "gcc" "CMakeFiles/tester.dir/src/ScrimParser.cpp.o.d"
+  "/home/diana/CLionProjects/Prog_RGB_333/src/Utils.cpp" "CMakeFiles/tester.dir/src/Utils.cpp.o" "gcc" "CMakeFiles/tester.dir/src/Utils.cpp.o.d"
+  "" "tester" "gcc" "CMakeFiles/tester.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
